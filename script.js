96022e03dc9e7f498287ac0d56791bd89cd84cf6
“Afaroj” <afaroj.geexu@gmail.com>
2022-11-11 18:38:51 +0530

    book, packages, services, gallery, review, contact, brands and footer section with swiper property

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ formBtn.addEventListener('click',()=>{
 formClose.addEventListener('click',()=>{
     loginForm.classList.remove('active');
 });
-
+// swiper
 videoBtn.forEach(btn =>{
     btn.addEventListener('click', ()=>{
         document.querySelector('.controls .active').classList.remove('active');
@@ -47,4 +47,46 @@ videoBtn.forEach(btn =>{
         let src = btn.getAttribute('data-src');
         document.querySelector('#video-slider').src = src;
     });
+});
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5,
+        },
+      },
 });
